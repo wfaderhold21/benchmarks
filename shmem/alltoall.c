8112cb4f3ee9df0692f9cf77cdf5ae728ed65cf2
Dr William Ferrol Aderholdt <waderholdt@c00.cs.mtsu.edu>
2019-06-06 15:39:05 -0500

    Add updated alltoall benchmark

--- shmem/alltoall.c
+++ shmem/alltoall.c
@@ -3,6 +3,20 @@
 
 #include <shmem.h>
 
+#include "cycles.c"
+
+#define MAX_ITER    100
+#define SKIP        10
+
+/* arbitrary data larger than 64 bits */
+struct data {
+    char data;
+};
+typedef struct data data_t;
+
+/*
+ * Linear all to all implementation
+ */
 void myalltoall(void * dest, const void * source, size_t nelems, size_t selems, 
                 int PE_start, int logPE_stride, int PE_size, long * pSync)
 {
@@ -17,48 +31,65 @@ void myalltoall(void * dest, const void * source, size_t nelems, size_t selems,
     shmem_sync(PE_start, logPE_stride, PE_size, pSync);
 }
 
-
 int main(void) 
 {
     int nr_elems;
     int my_pe, n_pes;
     long * pSync;
-    long * data;
-    long * alldata;
+    data_t * data;
+    data_t * alldata;
     int i = 0;
+    cycles_t * tstamp;
+    struct report_options report = {};
 
     shmem_init();
     my_pe = shmem_my_pe();
     n_pes = shmem_n_pes();
 
-    pSync = (long *) shmem_malloc(sizeof(long) * SHMEM_ALLTOALL_SYNC_SIZE);
+    tstamp = (cycles_t *) malloc(sizeof(cycles_t) * 2 * MAX_ITER);
 
+    pSync = (long *) shmem_malloc(sizeof(long) * SHMEM_ALLTOALL_SYNC_SIZE);
     for (i = 0; i < SHMEM_ALLTOALL_SYNC_SIZE; i++) {
         pSync[i] = SHMEM_SYNC_VALUE;
     }
 
-    for (i = 1; i < 4; i++) {
-        nr_elems = i;
-        int j = 0;
+    for (i = 0; i <= 20; i++) {
+        nr_elems = (1 << i);
+        int j = 0, k = 0, z = 0;
 
-        data = (long *) shmem_malloc(sizeof(long) * nr_elems);
-        alldata = (long *) shmem_malloc(sizeof(long) * nr_elems * n_pes);
+        data = (data_t *) shmem_malloc(sizeof(data_t) * nr_elems);
+        alldata = (data_t *) shmem_malloc(sizeof(data_t) * nr_elems * n_pes);
 
         for (j = 0; j < nr_elems; j++) {
-            data[j] = (long) my_pe * i + j;
+        //    data[j].nr = my_pe * nr_elems + j;
+            data[j].data = (long) my_pe * i + j;
         }               
 
-        myalltoall(alldata, data, nr_elems, sizeof(long), 0, 0, n_pes, pSync);
+        for (k = 0; k < MAX_ITER; k++) {
+            if (k >= SKIP) {
+                tstamp[z++] = get_cycles();
+            }
+            myalltoall(alldata, data, nr_elems, sizeof(data_t), 0, 0, n_pes, pSync);
+            if (k >= SKIP) {
+                tstamp[z++] = get_cycles();
+            }   
+        }
+
         if (shmem_my_pe() == 0) {
             printf("completed iteration %d\n", i);
-            for (j = 0; j < n_pes; j++) {
+/*            for (j = 0; j < n_pes; j++) {
                 int k = 0;
                 printf("%d: ", j);
                 for (k; k < nr_elems; k++) {
-                    printf("%lu ", alldata[j*nr_elems + k]);
+                    printf("%lu ", alldata[j*nr_elems + k].data);
                 }
                 printf("\n");
-            }
+            }*/
+
+            printf("Time with size: %lu:\n", sizeof(data_t) * nr_elems);
+            print_report(&report, MAX_ITER - SKIP, tstamp);
+
+
         }
 
         shmem_free(data);
