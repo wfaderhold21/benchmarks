2f110b17bd59432389b0c6ad01a1f541aa12bfc5
ferrol aderholdt <faderholdt@nvidia.com>
2021-11-18 14:55:12 -0800

    Update benchmark code

--- shmem/alltoall-generic/osh_bench_a2a.c
+++ shmem/alltoall-generic/osh_bench_a2a.c
@@ -12,31 +12,41 @@
 #include <sys/time.h>
 #include <limits.h>
 
-#define NR_ITER     10000
-#define SKIP        1000
+#define NR_ITER     1000
+#define SKIP        100
 
-int main(void)
+int main(int argc, char ** argv)
 {
     shmem_init();
     int me = shmem_my_pe();
     int npes = shmem_n_pes();
-    int count = 524288;
+    int count = 32768;
     long * pSync;
     long * pSync2;
+    long * pSync3;
     double * pWrk;
+    static long val = 9999;
     int64_t* dest = (int64_t*) shmem_malloc(count * npes * sizeof(int64_t));
     int64_t* source = (int64_t*) shmem_malloc(count * npes * sizeof(int64_t));
-    double min_latency, max_latency;
+    static double min_latency, max_latency;
     static double src_buff, dest_buff;
-    double start, end, total = 0.0;
+    static double total_time = 0.0;
+    static double start, end, total = 0.0;
+    int ppn = 1;
+
+    if (argc > 1) {
+        ppn = atoi(argv[1]);
+    }
 
     pSync = (long *) shmem_malloc(sizeof(long) * SHMEM_ALLTOALL_SYNC_SIZE);
     pSync2 = (long *) shmem_malloc(sizeof(long) * SHMEM_ALLTOALL_SYNC_SIZE);
+    pSync3 = (long *) shmem_malloc(sizeof(long) * SHMEM_REDUCE_SYNC_SIZE);
     pWrk = (double *) shmem_malloc(sizeof(double) * SHMEM_REDUCE_MIN_WRKDATA_SIZE);
 
-    for (int i = 0; i < npes; i++) {
+    for (int i = 0; i < SHMEM_ALLTOALL_SYNC_SIZE; i++) {
         pSync[i] = SHMEM_SYNC_VALUE;
         pSync2[i] = SHMEM_SYNC_VALUE;
+        pSync3[i] = SHMEM_SYNC_VALUE;
     }
 
     /* assign source values */
@@ -48,50 +58,59 @@ int main(void)
     }
 
     if (me == 0) {
-        printf("%-10s%15s%13s%13s%13s%13s\n", "Size", 
+        printf("%-10s%15s%13s%13s%13s%13s%13s\n", "Size", 
                                               "Bandwidth MB/s", 
-                                              "Agg BW MB/s", 
+                                              "Agg MB/s",
+                                              "Total BW",
                                               "Avg Latency", 
                                               "Min Latency", 
                                               "Max Latency");
     }
 
-    for (int k = 1; k <= 524288; k *= 2) {
+    for (int k = 1; k <= count; k *= 2) {
         double bandwidth = 0, agg_bandwidth = 0;
-       /* wait for all PEs to initialize source/dest */
-        shmem_barrier_all();
-
+        static double total_bw = 0, min = 0;
         min_latency = (double) INT_MAX;
         max_latency = (double) INT_MIN;
-
+        total = 0;
+        
         /* alltoall */
         for (int i = 0; i < NR_ITER; i++) {
-            long * pSync3 = (i & 2) ? pSync : pSync2;
+            long * a_psync = (i % 2) ? pSync : pSync2;
+            double b_start, b_end;
             start = MPI_Wtime();
-            shmem_alltoall64(dest, source, k, 0, 0, npes, pSync3);
+            shmem_alltoall64(dest, source, k, 0, 0, npes, a_psync);
             end = MPI_Wtime();
 
             if (i > SKIP) {
                 double time = end - start;
-                total += end - start;
+                total += time;// - (b_end - b_start);
                 if (time < min_latency) {
-                    min_latency = time;
+                    min = time;
                 } 
                 if (time > max_latency) {
                     max_latency = time;
                 }
             }
+            shmem_barrier_all();
         }
-        bandwidth = (npes / 1e6 * (NR_ITER - SKIP) * k * sizeof(uint64_t)) / (total);
-        src_buff = bandwidth;
-        shmem_double_sum_to_all(&dest_buff, &src_buff, 1, 0, 0, npes, pWrk, pSync); 
-        agg_bandwidth = dest_buff;
 
+        shmem_double_min_to_all(&min_latency, &min, 1, 0, 0, npes, pWrk, pSync2);
+        shmem_barrier_all();
+        shmem_double_sum_to_all(&total_time, &total, 1, 0, 0, npes, pWrk, pSync);
+        total_time = total_time / npes;
+        total_bw = (npes * (k * sizeof(uint64_t))) / min_latency;
+        bandwidth = (npes * (k * sizeof(uint64_t)) * (NR_ITER - SKIP)) / (total_time);
+        src_buff = bandwidth;
+        shmem_barrier_all();
+        shmem_double_sum_to_all(&dest_buff, &src_buff, 1, 0, 0, npes, pWrk, pSync3); 
+        agg_bandwidth = dest_buff;
         if (me == 0) {
             printf("%-10ld", k * sizeof(uint64_t));
-            printf("%15.2f", bandwidth);
-            printf("%13.2f", agg_bandwidth);
-            printf("%13.2f", (total * 1e6) / ((NR_ITER - SKIP)));
+            printf("%15.2f", bandwidth / (1024 * 1024));
+            printf("%13.2f", agg_bandwidth / (1024 * 1024));
+            printf("%13.2f", total_bw / (1024 * 1024));
+            printf("%13.2f", (total_time * 1e6) / ((NR_ITER - SKIP)));
             printf("%13.2f", min_latency * 1e6);
             printf("%13.2f\n", max_latency * 1e6);
         }
